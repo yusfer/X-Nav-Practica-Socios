752dd898b332d56c0d5388924d7091cc8191d4be
yusfer <yustasfernando@gmail.com>
2015-03-19 12:47:16 +0100

    Preparado tema accodion y tab

--- js/socios.js
+++ js/socios.js
@@ -5,29 +5,55 @@ jQuery(document).ready(function() {
 	});
 	
 	$("#mytim").click(function(){	//ASÍ CAMBIO CONTENIDO
-		$("#tabs-2").text("CARA PREPUCIO")
+		actualiza("mytimeline.json","#tabs-2")
 	});
 	$("#tim").click(function(){	//ASÍ CAMBIO CONTENIDO
-		actualiza("timeline.json")
+		actualiza("timeline.json","#tabs-1")
 	});
 
 });
 
-function actualiza(line){
+function actualiza(line,tabla){
 	aux=null;
+	
+	compruebo = $(tabla).accordion("instance")
+	
+	if(compruebo!=undefined){
+		$(tabla).accordion("destroy")
+		$(tabla).accordion("instance")
+	}
+	
 	$.getJSON(line,function(data){
 	
 		aux=data
 		
 		}).done(function(){
 		
-			list = "<ul>"
+			list = ""
 			for(i=0;i<aux.items.length;i++){
-				list = list + "<li>" + aux.items[i].Autor + "</li>"
+				list = list + preparotexto(aux.items[i])
 			}
-			list = list + "</ul>"
-			$("#tabs-1").html(list);
+			
+			$(tabla).html(list);
+			$(tabla).accordion({active: true});
 			
 		})
+	 
+}
+
+function preparotexto(item){
+	
+	texto = null;
+	list = "<ul>"
+	list = list + "<li>" + "<img class ='avatar' src="+item.Avatar+">" + "</li>"
+	list = list + "<li>" + item.Autor + "</li>"
+	list = list + "<li>" + item.Titulo + "</li></ul>"
+	
+	list2 = "<ul>"
+	list2 = list2 + "<li>" + item.Fecha + "</li>"
+	list2 = list2 + "<li>" + item.Contenido + "</li></ul>"
+	texto = "<h3>"+list+"</h3><div><p>" + list2 +"</p></div>"
+	
+	return texto
 	
 }
